cd10f0245bd79564007d3061a9f496b60c4ef52b
Francesca Giordano <fg6@sanger.ac.uk>
2017-04-11 15:08:16 +0100

    Adding untar and poretools for ONT

--- launchme.sh
+++ launchme.sh
@@ -22,12 +22,6 @@ else
     strains=( s288c sk1 cbs n44 )
 fi
 
-
-function validate_url(){
-  if [[ `wget -S --spider $1  2>&1  | grep exists` ]]; then echo "true"; else echo "false"; fi
-}
-
-
 ##########################################
 ####### download some utilities ##########
 ##########################################
@@ -36,6 +30,7 @@ mkdir -p $thisdir/src
 cd $thisdir/src
 
 if [ ! -f locpy/bin/activate ]; then
+    cd $thisdir/src
     pyversion=`python -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(major);'`
     minor=`python -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(minor);'`
 
@@ -71,13 +66,26 @@ fi
 source $thisdir/src/locpy/bin/activate
 
 if [ ! -d  $thisdir/src/poretools ] ; then
+    # used to extract fastq from ont fast5
     cd $thisdir/src/
     git clone https://github.com/arq5x/poretools.git
     cd poretools/
     git reset --hard 4e04e25f22d03345af97e3d37bd8cf2bdf457fc9   
     python setup.py install
 fi
-	
+
+#if [ ! -d  $thisdir/src/pbh5tools ] ; then
+ #   #used to extract fastq from pacbio hdf5 
+#    cd $thisdir/src
+#    source $thisdir/src/locpy/bin/activate
+#    pip install pysam
+ #   pip install h5py
+#    git clone https://github.com/PacificBiosciences/pbh5tools.git
+#    cd pbh5tools
+#    python setup.py install
+#fi
+
+
 
 if [ ! -d  $thisdir/src/fq2fa ] ; then
     ## fastq 2 fasta
@@ -95,7 +103,7 @@ fi
 
 echo "   ... ready!"
 
-
+# if 'none' option: all done, end here
 if [[ ${#strains[@]} -eq 0 ]]; then exit; fi
 
 
@@ -108,7 +116,7 @@ if [[ ${#strains[@]} -eq 0 ]]; then exit; fi
 ########## Download data from ENA #########
 ###########################################
 cd $thisdir
-source locpy/bin/activate
+source $thisdir/src/locpy/bin/activate
 
 
 #******************* ONT ******************* #
@@ -140,19 +148,43 @@ for strain in "${strains[@]}"; do
 	    echo untar
 	fi
 	    	    
-	if [ ! -f $fold.fastq ]; then
-	    echo poretools fastq --type 2D $fold  ">" $fold.fastq
+	if [ ! -f $fold\_pass2D.fastq ]; then
+	    
+	    fast5pass=$fold/reads/downloads/pass
+	    fast5fail=$fold/reads/downloads/fail
+
+	    if ! ls $fast5pass  &> /dev/null; then
+		echo "  " no fast5 found! $fast5pass
+		nfiles=0
+	    else
+		nfiles=`ls $fast5pass | wc -l`
+	    fi
+	    
+	    if [ $nfiles -gt 0 ]; then
+		#echo poretools
+		poretools fastq --type 2D $fast5pass > $fold\_pass2D.fastq
+		if [ $strain == "s288c" ]; then
+		    #echo poretools fail
+		    poretools fastq --type 2D $fast5fail > $fold\_fail2D.fastq
+		fi
+	    else
+		echo no fast5 found! $fast5pass
+	    fi
 	fi ## poretools
 	    
     done # runs
     
-    echo all done, now merge
-    fqs=`ls *fastqs | wc -l` 
-    
-    if [ ! -z ${fqs-x} ]; then  
-	echo ok $fqs
+    if ! ls *fastq  &> /dev/null; then
+	fqs=0
     else
-	echo no fastqs
+	fqs=`ls *fastq | wc -l` 
+    fi
+
+
+    if [ $fqs -eq 0 ]; then  
+	echo; echo " Error! no ONT fastqs found or creaed"  $strain
+    else
+	echo " Merging " $fqs "fastqs for the " $strain " strain"
     fi
 
 
