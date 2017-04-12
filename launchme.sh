cc4b546a260d3eb92ff93af3f2401cb22520be35
Francesca Giordano <fg6@sanger.ac.uk>
2017-04-12 11:47:10 +0100

    ont and pacbio data preparation ready

--- launchme.sh
+++ launchme.sh
@@ -74,18 +74,18 @@ if [ ! -d  $thisdir/src/poretools ] ; then
     python setup.py install
 fi
 
-#if [ ! -d  $thisdir/src/pbh5tools ] ; then
- #   #used to extract fastq from pacbio hdf5 
-#    cd $thisdir/src
-#    source $thisdir/src/locpy/bin/activate
-#    pip install pysam
- #   pip install h5py
-#    git clone https://github.com/PacificBiosciences/pbh5tools.git
-#    cd pbh5tools
-#    python setup.py install
-#fi
-
-
+if [ ! -d  $thisdir/src/pbh5tools ] ; then
+   #used to extract fastq from pacbio hdf5 
+    cd $thisdir/src
+    source $thisdir/src/locpy/bin/activate
+    which python
+    pip install pysam
+    pip install h5py
+    pip install pbcore
+    git clone https://github.com/PacificBiosciences/pbh5tools.git
+    cd pbh5tools
+    python setup.py install
+fi
 
 if [ ! -d  $thisdir/src/fq2fa ] ; then
     ## fastq 2 fasta
@@ -121,7 +121,7 @@ source $thisdir/src/locpy/bin/activate
 
 #******************* ONT ******************* #
 
-folder=$thisdir/fastqs/ontfast5
+folder=$thisdir/fastqs/ont
 	
 mkdir -p $folder
 cd $folder
@@ -130,94 +130,146 @@ for strain in "${strains[@]}"; do
     mkdir -p $folder/$strain
     cd  $folder/$strain
 
-    thislist=ont${strain}[@]
-    for tarfile  in "${!thislist}"; do
-	file=$ontftp/$tarfile
-	fold=$(basename "$tarfile" .tar.gz)
+    if [ ! -f $strain\_pass2D.fastq ]; then  ## only if fastq file is not there already
 
-	if [ ! -f $tarfile ] && [ ! -f $fold.fastq ] ; then
-	    if [[ `wget -S --spider $file 2>&1  | grep exists` ]]; then
-	    	wget $ontftp/$tarfile
-	    else 
-		echo "Could not find url " $file
-	    fi
-	fi
-
-	if [ ! -d $fold ] && [ ! -f $fold.fastq ] ; then
-	    tar -xvzf $tarfile
-	    echo untar
-	fi
-	    	    
-	if [ ! -f $fold\_pass2D.fastq ]; then
+	thislist=ont${strain}[@]
+	for tarfile  in "${!thislist}"; do
+	    file=$ontftp/$tarfile
+	    fold=$(basename "$tarfile" .tar.gz)
 	    
-	    fast5pass=$fold/reads/downloads/pass
-	    fast5fail=$fold/reads/downloads/fail
-
-	    if ! ls $fast5pass  &> /dev/null; then
-		echo "  " no fast5 found! $fast5pass
-		nfiles=0
-	    else
-		nfiles=`ls $fast5pass | wc -l`
-	    fi
-	    
-	    if [ $nfiles -gt 0 ]; then
-		#echo poretools
-		poretools fastq --type 2D $fast5pass > $fold\_pass2D.fastq
-		if [ $strain == "s288c" ]; then
-		    #echo poretools fail
-		    poretools fastq --type 2D $fast5fail > $fold\_fail2D.fastq
+	    if [ ! -f $tarfile ] && [ ! -f $fold\_pass2D.fastq ] ; then
+		if [[ `wget -S --spider $file 2>&1  | grep exists` ]]; then
+	    	    wget $ontftp/$tarfile
+		else 
+		    echo "Could not find url " $file
 		fi
-	    else
-		echo no fast5 found! $fast5pass
 	    fi
-	fi ## poretools
+
+	    if [ ! -d $fold ] && [ ! -f $fold\_pass2D.fastq ] ; then
+		tar -xvzf $tarfile
+		echo untar
+	    fi
 	    
-    done # runs
+	    if [ ! -f $fold\_pass2D.fastq ]; then
+		
+		fast5pass=$fold/reads/downloads/pass
+		fast5fail=$fold/reads/downloads/fail
+		
+		if ! ls $fast5pass  &> /dev/null; then
+		    echo "  " no fast5 found! $fast5pass
+		    nfiles=0
+		else
+		    nfiles=`ls $fast5pass | wc -l`
+		fi
+		
+		if [ $nfiles -gt 0 ]; then
+		    #echo poretools
+		    poretools fastq --type 2D $fast5pass > $fold\_pass2D.fastq
+		    if [ $strain == "s288c" ]; then
+		    #echo poretools fail
+			poretools fastq --type 2D $fast5fail > $fold\_fail2D.fastq
+		    fi
+		    
+		else
+		    echo no fast5 found! $fast5pass
+		fi
+	    fi ## poretools
+	done # runs
     
-    if ! ls *fastq  &> /dev/null; then
-	fqs=0
-    else
-	fqs=`ls *fastq | wc -l` 
+    
+
+	if ! ls *fastq  &> /dev/null; then
+	    fqs=0
+	else
+	    fqs=`ls *fastq | wc -l` 
+	fi
+	
+
+	if [ $fqs -eq 0 ]; then  
+	    echo; echo " Error! no ONT fastqs found or creaed"  $strain
+	else
+	    echo " Merging " $fqs "fastqs for the " $strain " strain"
+	    
+	    for f in *_pass2D.fastq; do 
+		cat $f >> $strain\_pass2D.fastq
+	    done 
+	    chmod -w $strain\_pass2D.fastq
+
+	    
+	    if [ $strain == "s288c" ]; then
+	        cp $strain\_pass2D.fastq $strain\_all2D.fastq
+		for f in *fail2D.fastq; do 
+		    cat $f >> $strain\_all2D.fastq
+		done
+		chmod -w $strain\_all2D.fastq
+	    fi
+	fi
+    
+    fi ## if ! global fastq file 
+
+    # clean all except fastq files before starting next strain
+    if [ -f $strain\_pass2D.fastq ]; then 
+	dd=`ls -I "*fastq" | wc -l`
+	if [ $dd -gt 0 ]; then  
+	    rm -r $(ls -I "*fastq")
+	fi
     fi
-
-
-    if [ $fqs -eq 0 ]; then  
-	echo; echo " Error! no ONT fastqs found or creaed"  $strain
-    else
-	echo " Merging " $fqs "fastqs for the " $strain " strain"
-    fi
-
-
 done # strain
 
-exit
 #******************* PacBio ******************* #
 
-folder=$thisdir/fastqs/pbh5
+folder=$thisdir/fastqs/pacbio
 mkdir -p $folder
 cd $folder
 
 
-for strain in "${strains[@]}"; do
+
+for strain in "${strains[@]}"; do   ## loop on strains
     mkdir -p $folder/$strain
     cd  $folder/$strain
 
-    runs=pb${strain}[@]
 
-    for run in "${!runs}"; do
-	mkdir -p  $folder/$strain/$run
-	cd  $folder/$strain/$run
+    if [ ! -f $strain\_pacbio.fastq ]; then  ##  if fastq file is not there already
 
-	thislist=pb$strain\_${run}[@]
-	for tarfile  in "${!thislist}"; do
-	    if [[ `wget -S --spider $tarfile 2>&1  | grep exists` ]]; then
-	        #wget $ontftp/$tarfile
-		echo $strain $tarfile ok
-	    else 
-		echo "Could not find url " $tarfile
-	    fi
-	done
-    done
+	runs=pb${strain}[@]
+	for run in "${!runs}"; do   ## loop over pacbio runs
+	    
+	    thislist=pb$strain\_${run}[@]
+	    for tarfile  in "${!thislist}"; do # loop over pacbio runs
+		if [ ! -f $(basename $tarfile) ]; then  # download if file is not there already
+		    if [[ `wget -S --spider $tarfile 2>&1  | grep exists` ]]; then
+			wget $tarfile
+		    else 
+			echo "Could not find url " $tarfile
+		    fi
+		fi
+	    done  # download each file in a run
+
+	    # files are downloaded, now extract fastq 	    
+	    for file in *.bas.h5; do
+		if [ ! -f $(basename $file .bas.h5).fastq ]; then
+		    python $thisdir/src/pbh5tools/bin/bash5tools.py --minLength 500 --minReadScore 0.8000 --readType subreads --outType fastq $file 
+		fi
+	    done
+	    
+	done  ## runs
+
+	# fastq per run ready: now merge them in a single file
+	for f in *.fastq; do 
+	    cat $f >> $strain\_pacbio.fastq
+	done 
+	chmod -w $strain\_pacbio.fastq
+	
+   fi ## if ! global fastq file 
+
+   # if all successful: clean all except fastq files before starting next strain
+   if [ -f $strain\_pacbio.fastq ]; then 
+       dd=`ls -I "*fastq" | wc -l`
+       if [ $dd -gt 0 ]; then  
+	   rm -r $(ls -I "*fastq")
+       fi
+   fi
+   
 done
 
 exit
