81ac9ae5ce975e16b52f7afb0300cd8ec4b0399d
Francesca Giordano <fg6@sanger.ac.uk>
2017-04-20 09:52:38 +0100

    adding deepcheck

--- utils/deepcheck.sh
+++ utils/deepcheck.sh
@@ -13,186 +13,79 @@ fi
 platforms=( ont pacbio miseq )
 
 
-#pass2D, all2D
-ont_s288c=( 382637184 737451072 )
-ont_sk1=( 51002440 )
-ont_cbs=( 109308008 )
-ont_n44=( 129796408 )
-#whole, 31X_ont_emu
-pacbio_s288c=( 1462662016  375115552)
-pacbio_sk1=( 3018814976 )
-pacbio_cbs=( 1639253632 )
-pacbio_n44=( 1793698048 )
-#reads_1, reads_2
-miseq_s288c=( 497770048 497770048 )
-miseq_sk1=( 492408000 492408000 )
-miseq_cbs=( 477162912 477162912 )
-miseq_n44=( 504046656 504046656 )
-
-#file name details:
-ontn=( pass2D all2D )
-pacbion=( pacbio pacbio_ontemu_31X )
-
 if [ ! -f  $thisdir/utils/src/n50/n50 ] ; then
     echo Some utilities are missing, please run ./launchme.sh install
     exit
 fi
 
-files=()
-bases=()
-
-filename="$thisdir/utils/test.list"
-while read -r f1 f2 || [[ -n "$f1" ]]; do
-    files+=($f1)
-    bases+=($f2)
-done < "$filename"
+platforms=( ont )
+fqlist="$thisdir/utils/fastq_bases.list"
+#fqlist="$thisdir/utils/test.list"
 
 
-echo; echo
-ii=-1
-for file in "${files[@]}"; do
-    ii=$(($ii+1))
-    echo $ii  $file  ${bases[$ii]}
-
-    check=bases[$ii]
-
-    ls $thisdir/$file
-
-    if [ -f $thisdir/$file ]; then
-	thischeck=`$thisdir/utils/src/n50/n50 $thisdir/$file | awk '{print $2}'`
-
-	if [ "${!check}" = "$thischeck" ]; then echo "    " $file  OK;
-	    else 
-		echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-		echo "     !!!! " $file  NOT OK ;  
-		echo "     !!!!    Something went wrong during the fastq preparation "
-		echo "     !!!!    Please "; 
-		echo "     !!!!       1. remove the fastq file: $ rm -f " $file
-		echo "     !!!!       2. relaunch: $ ./launchme.sh download "  $strain
-		echo "     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " 
-
-		#errors=$(($errors+1))
-	    fi
-    else
-        echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-	echo "     Cannot find fastq file for" $strain: $file ;
-	echo "          Download it with: "  $ ./launchme.sh download $strain 
-	    #missing=$(($missing+1))
-    fi
-
-
-done
-
-
-exit
-
-errors=0
-missing=0
 for platform in "${platforms[@]}"; do  
-
     echo; echo " *****************"; echo  "   " $platform files:; echo " *****************"; 
-    folder=fastqs/$platform
 
     for strain in "${strains[@]}"; do   
         echo  "   strain=" $strain
 
-	if [ $platform != "miseq" ]; then name=${platform}n[0]; fi
-	file=$folder/$strain/$strain\_"${!name}".fastq
-	if [ $platform == "miseq" ]; then 
-	    file=$folder/$strain\_1.fastq; 
-	    file2=$folder/$strain\_2.fastq; 
-	fi
-	
-	if [ -f $file ]; then 
-	    check=$platform\_${strain}
-	    thischeck=`$thisdir/utils/src/n50/n50 $file | awk '{print $2}'`
+	errors=0
+	missing=0
+	entries=0
 
-	    if [ "${!check}" = "$thischeck" ]; then echo "    " $thistrain $file  OK;
-	    else 
-		echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-		echo "     !!!! " $thistrain $file  NOT OK ;  
-		echo "     !!!!    Something went wrong during the fastq preparation "
-		echo "     !!!!    Please "; 
-		echo "     !!!!       1. remove the fastq file: $ rm -f " $file
-		echo "     !!!!       2. relaunch: $ ./launchme.sh download "  $strain
-		echo "     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " 
+	while read -r f1 f2 || [[ -n "$f1" ]]; do
+	    file=$f1
+	    check=$f2
+	    entries=$(($entries+1))
 
-		errors=$(($errors+1))
-	    fi
-	else
-            echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-	    echo "     Cannot find fastq file for" $strain: $file ;
-	    echo "          Download it with: "  $ ./launchme.sh download $strain 
-	    missing=$(($missing+1))
-	fi
+	    
+            if [ -f $thisdir/$file ]; then
+		thischeck=`$thisdir/utils/src/n50/n50 $thisdir/$file | awk '{print $2}'`
 
-	if [ $platform == "miseq" ]; then 
-	    if [ -f $file ]; then 
-		check=$platform\_${strain}[1]
-		thischeck=`$thisdir/utils/src/n50/n50 $file2 | awk '{print $2}'`
-		
-		if [ "${!check}" = "$thischeck" ]; then echo "    " $thistrain $file2  OK;
-		else 
-		    echo;echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-		    echo "     !!!! " $thistrain $file  NOT OK ;  
+		if [ $check != "$thischeck" ]; then 
+		    echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
+		    echo "     !!!! " $file  NOT OK ;  
 		    echo "     !!!!    Something went wrong during the fastq preparation "
 		    echo "     !!!!    Please "; 
 		    echo "     !!!!       1. remove the fastq file: $ rm -f " $file
 		    echo "     !!!!       2. relaunch: $ ./launchme.sh download "  $strain
 		    echo "     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " 
-                    errors=$(($errors+1))
+		    
+		    echo folder:
+		    ls $(basename $file .fastq)
+		    echo tar:
+		    ls $(basename $file .fastq).tar.gz
+		    errors=$(($errors+1))
+
 		fi
 	    else
-                echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-		echo "     Cannot find fastq file for" $strain\: $file2 ;
-		echo "          Download it with: "  $ ./launchme.sh download $strain
-                missing=$(($missing+1))
+		echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
+		echo "     Cannot find fastq file for" $strain: $file ;
+
+		echo folder:
+		echo $(basename $file _pass2D.fastq)
+		echo tar:
+		echo $(basename $file _pass2D.fastq).tar.gz
+		errors=$(($errors+1))
+
+
+	        #echo "          Download it with: "  $ ./launchme.sh download $strain 
+	        missing=$(($missing+1))
 	    fi
-	fi
+
+	done < <( grep $strain "$fqlist" | grep $platform )
+
+
 	
-	if [ $strain == "s288c" ] && [ $platform != "miseq" ]; then
-	    name=${platform}n[1]
-	    file=$folder/$strain/$strain\_"${!name}".fastq
-	   
-	    if [ -f $file ]; then 
-		check=$platform\_${strain}[1]
-		thischeck=`$thisdir/utils/src/n50/n50 $file | awk '{print $2}'`
-		if [ "${!check}" = "$thischeck" ]; then echo "    " $thistrain $file  OK;
-		else 
-		    echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-		    echo "     !!!! " $thistrain $file  NOT OK ;  
-		    echo "     !!!!    Something went wrong during the fastq preparation "
-		    echo "     !!!!    Please "; 
-		    echo "     !!!!       1. remove the fastq file: $ rm -f " $file
-		    echo "     !!!!       2. relaunch: $ ./launchme.sh download "  $strain 
-		    echo "     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " 
-                    errors=$(($errors+1))
-		fi
+	if [[ $entries > 0 ]]; then
+	    if [[ $missing != 0 ]] || [[ $errors != 0 ]]; then
+		echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
+		echo "      Some files failed to download or un-compress properly "
+		echo "     Please check the warnings above and follow instructions "
+		echo "     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " 
 	    else
-                echo; echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-		echo "     Cannot find fastq file for" $strain\: $file ;
-		echo "          Download it with: "  $ ./launchme.sh download $strain 
-                missing=$(($missing+1))
+		echo "   Good news: all your files appear to be fine "
 	    fi
 	fi
     done
 done
-
-
-echo;echo 
-
-if [[ $missing != 0 ]] || [[ $errors != 0 ]]; then
-        echo "     !!!!!!!!!!!!!!!!!!!!!! Warning !!!!!!!!!!!!!!!!!!!!!!!! " 
-        echo "      Some files failed to download or un-compress properly "
-        echo "     Please check the warnings above and follow instructions "
-        echo "     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " 
-else
-        echo " All your files appear to be fine "
-fi
-
-
-
-
-
-
-
