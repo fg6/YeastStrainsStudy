fff3b15fadbfaf0b33f95e736b5be2c5a2bce991
Francesca Giordano <fg6@sanger.ac.uk>
2017-04-26 09:31:59 +0100

    changed folder

--- pipelines/miniasm.sh
+++ pipelines/miniasm.sh
@@ -38,7 +38,7 @@ fi
 
 
 if [ $platform == 'ont' ]; then
-	reads=$thisdir/../fastqs/ont/$strain\_pass2D.fastq
+	reads=$thisdir/../fastqs/ont/$strain/$strain\_pass2D.fastq
 	if [ ! -z ${cov-x} ] && [ $cov == 'allX' ]; then
         	echo 'allX option valid for pacbio s288c only!'
         	exit 1
@@ -65,10 +65,10 @@ else
                         echo '31X option valid for pacbio s288c only!'
                         exit 1
                 elif [ $cov == '31X' ] && [ $strain == 's288c' ]; then	
-	        	reads=$thisdir/../fastqs/pacbio/s288c_pacbio_ontemu_31X.fastq
+	        	reads=$thisdir/../fastqs/pacbio/s288c/s288c_pacbio_ontemu_31X.fastq
 			outdir=$outdir\_31X_ONTemu			
 		else
-                	reads=$thisdir/../fastqs/pacbio/$strain\_pacbio.fastq               	
+                	reads=$thisdir/../fastqs/pacbio/$strain/$strain\_pacbio.fastq               	
 		fi	
 	else	
 		if [ $strain == 's288c' ]; then
@@ -77,7 +77,7 @@ else
                 	echo "   cov: only for pacbio s288c: choose coverage sample '31X' or 'allX' "
                 	exit 1
         	else
-                	reads=$thisdir/../fastqs/pacbio/$strain\_pacbio.fastq                        
+                	reads=$thisdir/../fastqs/pacbio/$strain/$strain\_pacbio.fastq                        
 		fi
 
 	fi
